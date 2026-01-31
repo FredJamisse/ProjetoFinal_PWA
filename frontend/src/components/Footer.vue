0b451af1be335ad387b109a594ba74d97c6a8658
FredJamisse <fredjamisse@gmail.com>
2026-01-31 22:23:12 +0200

    Made few improvements and bug fixes

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="bg-light text-center text-muted py-3 mt-5 border-top">
+    <div class="container">
+      <small>
+        Frederico Jamisse | Programação Web Avançada | 2025/2026
+      </small>
+    </div>
+  </footer>
+</template>
